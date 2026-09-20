dd84f40b9acfa395cd911585111bff0ac56bbfbe
Kalpana15 <33325008+Kalpana15@users.noreply.github.com>
2026-09-20 13:05:27 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.Length - 1;
+        while (low < high){
+            int sum = numbers[low]+ numbers[high];
+            if (sum == target){
+                return new int[] {low+1, high+1};
+            }
+            else if (sum < target){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
